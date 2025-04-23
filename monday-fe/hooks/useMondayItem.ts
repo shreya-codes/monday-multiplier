f3a962d7f1c931c0ba54651552a668b16671d3f5
Shreya Shrestha <shreya.shrestha@monotype.com>
2025-04-24 01:11:30 +0545

    remove unnecessary logs

--- monday-fe/hooks/useMondayItem.ts
+++ monday-fe/hooks/useMondayItem.ts
@@ -1,7 +1,6 @@
 import { useEffect, useState } from 'react'
 import mondaySdk from 'monday-sdk-js'
 import { MondayContext,MondayItem } from '../src/types/monday'
-import logger from '@utils/logger'
 const monday = mondaySdk()
 
 
@@ -26,7 +25,6 @@ const useMondayItem = () => {
           boardId: boardId
         })
       } catch (error) {
-        logger.error('Error getting Monday item:', error)
         setError(error instanceof Error ? error.message : 'Unknown error occurred')
       }
     }
